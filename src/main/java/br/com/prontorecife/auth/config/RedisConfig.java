b7bc22389195a03a7b4db00880c1b593b0154633
Ben-SS1 <gandao2013@gmail.com>
2024-12-04 17:39:29 -0300

    Ajustes: Adição de dependencias no pom.xml, ajustes no aplication properties

--- src/main/java/br/com/prontorecife/auth/config/RedisConfig.java
+++ src/main/java/br/com/prontorecife/auth/config/RedisConfig.java
@@ -7,15 +7,15 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@Configuration
+//@Configuration
 public class RedisConfig {
 
-    @Bean
+  /*  @Bean
     public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<?, ?> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
-    }
+    }*/
 }
