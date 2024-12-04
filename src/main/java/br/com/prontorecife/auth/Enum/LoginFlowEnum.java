e72345f82ef377b66bf817e934ecf1ce9b82aad6
Ben-SS1 <gandao2013@gmail.com>
2024-12-04 00:43:37 -0300

    Adição: Arquivo LoginFlowEnum

---
+++ src/main/java/br/com/prontorecife/auth/Enum/LoginFlowEnum.java
@@ -0,0 +1,21 @@
+package br.com.prontorecife.auth.Enum;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum LoginFlowEnum {
+    CRM,
+    CPF,
+    CNPJ;
+
+    @JsonValue
+    @Override
+    public String toString() {
+        return name();
+    }
+
+    @JsonCreator
+    public static LoginFlowEnum fromValue(String value) {
+        return LoginFlowEnum.valueOf(value.toUpperCase());
+    }
+}
