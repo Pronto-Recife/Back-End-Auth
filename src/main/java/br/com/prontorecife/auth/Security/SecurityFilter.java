3722b6cb95a4d5c7b229172b10b816b7b3c11e4a
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2024-12-09 01:23:40 -0300

    Ajuste nos imports e exports

--- src/main/java/br/com/prontorecife/auth/Security/SecurityFilter.java
+++ src/main/java/br/com/prontorecife/auth/Security/SecurityFilter.java
@@ -2,7 +2,6 @@ package br.com.prontorecife.auth.Security;
 
 import br.com.prontorecife.auth.Exceptions.AuthorizationHeaderException;
 import br.com.prontorecife.auth.Service.TokenService;
-import br.com.prontorecife.auth.Exceptions.CustomException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,10 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
